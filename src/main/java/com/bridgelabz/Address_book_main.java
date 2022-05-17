2e7e9fb786bd1bc992fca036fb363be863c2007d
DhanashriNerkar <dhanashrinerkar2@gmail.com>
2022-05-18 00:12:27 +0530

    added:Message added

---
+++ src/main/java/com/bridgelabz/Address_book_main.java
@@ -0,0 +1,12 @@
+package com.bridgelabz;
+
+public class Address_book_main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("WELCOME TO ADDRESS BOOK");
+
+	}
+
+}
